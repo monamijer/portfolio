c55dc1b84418a2b7dfab79469e6a51d962df3960
Monami Jerome <153171228+monamijer@users.noreply.github.com>
2026-03-20 08:53:01 +0200

    fix main js
    remove bootstrap  bundler

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import '../css/style.css'
-
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // Dark mode
+  // -------- Dark mode toggle --------
   const toggle = document.querySelector("#themeToggle");
+  if (toggle) {
+    // Vérifier si un thème a été sauvegardé
+    if (localStorage.getItem("theme") === "dark") {
+      document.body.classList.add("dark");
+      toggle.textContent = "Light Mode";
+    }
 
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
 
-
-  // Date auto
+      // Save the theme in localStorage
+      if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        toggle.textContent = "Light Mode";
+      } else {
+        localStorage.setItem("theme", "light");
+        toggle.textContent = "Dark Mode";
+      }
+    });
+  }
   const date = document.querySelector(".date");
   if (date) {
     date.textContent = new Date().getFullYear();
   }
-
-  
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
-}
-
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-
-  // Sauvegarder
-  if (document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-});
 
 });
