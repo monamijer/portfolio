f6505950d53f00ba367b584c7c7c3e864f57233c
Monami Jerome <153171228+monamijer@users.noreply.github.com>
2026-03-20 08:48:43 +0200

    fix mainjs

--- js/main.js
+++ js/main.js
@@ -9,16 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
   // Dark mode
   const toggle = document.querySelector("#themeToggle");
 
-  if (toggle) {
-    toggle.addEventListener("click", () => {
-      document.body.classList.toggle("dark");
-    });
-  }
+
 
   // Date auto
   const date = document.querySelector(".date");
   if (date) {
     date.textContent = new Date().getFullYear();
   }
+
+  
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Sauvegarder
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
 
 });
